fd061d247c5f6e3f72c766e3c8cc9c511b1cffa5
Erica <ericawebdev@gmail.com>
2026-01-21 18:07:04 -0500

    Fix a few things in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
 echo "Running database migrations..."
 alembic upgrade head
 
 echo "Starting API server..."
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
+echo "Port is ${PORT:?PORT environment variable is not set}"
+exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT" --log-level info
